b844f8fc64593da332cdfa82b5876d5d5b46f0e8
Anton Heryanto Hasan <anton.heryanto@gmail.com>
2024-01-02 21:38:21 +0800

    uses array of integer

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,24 +1,35 @@
 <script lang="ts">
-    import { times } from "$lib/jadualSholat";
+    import { timeRefs, hijri, times } from "$lib/jadualSholat";
+    
     function getDayIndex() : number {
         let now = new Date();
-        let start = new Date(now.getFullYear(), 0, 0);        
+        let start = new Date(now.getFullYear(), 0, 0);
         let diff = now.getTime() - start.getTime();
         let oneDay = 1000 * 60 * 60 * 24;
         let day = Math.floor(diff / oneDay);
         // console.log('Day of year: ' + day);
         return day - 1;
     }
-    let i = getDayIndex();
-    let time = times[i];
+
+    function getTime(hour: number, minute: number): String {
+        let nowD = new Date();
+        let nowH = new Date(nowD.getFullYear(), nowD.getMonth(), nowD.getDay(), hour);
+        let now = new Date(nowH.getTime() + minute * 60000);
+        //return `${now.getHours()}:${now.getMinutes()}`;
+        return now.toLocaleTimeString([], {hour: '2-digit', minute:'2-digit'});
+    }
+    
+    let i = getDayIndex() * 9;
+    let hijr = times.slice(i, i+3);
+    let time = times.slice(i+3, i+3+6);
 </script>
 
 <h1>Jadual Solat</h1>
-<h2> {Date()}</h2>
+<h3> {Date()}</h3>
+<h3> {hijr[0]} {hijri[hijr[1]]} {hijr[2]} Hijri</h3>
 <table>
 <thead>
     <tr>
-        <th>Tarikh</th>
         <th>Imsak</th>
         <th>Subuh</th>
         <th>Syuruk</th>
@@ -30,8 +41,9 @@
 </thead>
 <tbody>
     <tr>
-    {#each time as m}
-        <td>{m}</td>
+        <td>{getTime(timeRefs[0], time[0]-10)}</td>
+    {#each time as m, i}
+        <td>{getTime(timeRefs[i], time[i])}</td>
     {/each}
     </tr>
 </tbody>
